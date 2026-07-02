0524aa36360d577757e8b8ace82cb9c0bef22580
Fides <caalex321@gmail.com>
2026-07-02 11:33:20 -0400

    chapter 3 lesson 1

--- src/textnode.py
+++ src/textnode.py
@@ -39,3 +39,23 @@ def text_node_to_html_node(text_node:TextNode) -> LeafNode:
     if text_node.text_type == TextType.IMAGE:
         return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
     raise ValueError(f"invalid {text_node.text_type}")
+
+def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
+    nl = []
+    for x in old_nodes:
+        nl2 = []
+        if x.text_type != TextType.TEXT:
+            nl.append(x)
+            continue
+        tl = x.text.split(delimiter)
+        if len(tl) % 2 == 0:
+            raise ValueError("no matching delimeter for close")
+        for y in range(0, len(tl)):
+            if tl[y] == "":
+                continue
+            if y % 2 == 0:
+                nl2.append(TextNode(tl[y], TextType.TEXT))
+            else:
+                nl2.append(TextNode(tl[y], text_type))
+        nl.extend(nl2)
+    return nl
